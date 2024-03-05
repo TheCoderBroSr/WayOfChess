3cd3d8d6e598f2b78679ab081ad87997caab5c4a
TheCoderBroSr <arhantsurajkumar@gmail.com>
2024-03-05 21:10:20 +0530

    Adding templates and style with chessboard implementation

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app(test_config=None):
     # create and configure the app
@@ -24,8 +24,8 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
     return app
