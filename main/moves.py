f775fd991d63caf48b8254ad5c139a04a911aa20
TheCoderBroSr <arhantsurajkumar@gmail.com>
2024-03-06 12:37:13 +0530

    Adding a helper module to read FEN strings

---
+++ main/moves.py
@@ -0,0 +1,35 @@
+def read_FEN(fen_str):
+    '''
+    FEN -> Forsyth-Edwards Notation
+
+    eg. rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2
+
+    uppercase -> white
+    lowercase -> black
+    / -> next row
+    [1-8] -> no. of spaces
+    '''
+
+    piece_position = {}
+
+    position, player_to_move, castle_ability, en_pass_target_sqr, half_move, full_move = fen_str.split()
+
+    row_num = 8
+    for row in position.split("/"):
+        col_num = 0
+
+        for box in row:
+            print(row_num, col_num)
+            if box.isdigit():
+                col_num += int(box)
+
+            elif box.isupper():
+                piece_position[chr(col_num + 65) + str(row_num)] = box.lower() + 'l'
+            else:
+                piece_position[chr(col_num + 65) + str(row_num)] = box + 'd'
+
+            col_num += 1
+
+        row_num -= 1
+
+    return piece_position
